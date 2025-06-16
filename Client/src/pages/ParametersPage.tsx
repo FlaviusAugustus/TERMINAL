790c952f7c4a72691a233a2cb745d7b22572d91b
Hubert Wajda <hubertwajda@Huberts-MacBook-Air.local>
2025-06-17 00:01:44 +0200

    create new page and reorginize sidebaritems to maintain constistency

---
+++ Client/src/pages/ParametersPage.tsx
@@ -0,0 +1,36 @@
+import TableLayout from "./layouts/TableLayout";
+
+const ParametersPage = () => {
+
+  return (
+    <TableLayout>
+      TEST
+      {/*<ComponentOrLoader*/}
+      {/*  isLoading={dataQueryParameters.isLoading}*/}
+      {/*  loader={<Loader />}*/}
+      {/*>*/}
+      {/*  <Parameters*/}
+      {/*    Parameters={dataQueryParameters.data}*/}
+      {/*    sorting={sorting}*/}
+      {/*    pagination={pagination}*/}
+      {/*    setSorting={setSorting}*/}
+      {/*    setPagination={setPagination}*/}
+      {/*    onDelete={handleDelete}*/}
+      {/*    onEdit={changeSampleDetails}*/}
+      {/*  />*/}
+      {/*</ComponentOrLoader>*/}
+      {/*<ComponentOrLoader*/}
+      {/*  isLoading={dataQuerySampleDetails.isLoading}*/}
+      {/*  loader={<Loader />}*/}
+      {/*>*/}
+      {/*  <SampleDetails*/}
+      {/*    sample={dataQuerySampleDetails.data}*/}
+      {/*    open={detailsOpen}*/}
+      {/*    openChange={setDetailsOpen}*/}
+      {/*  />*/}
+      {/*</ComponentOrLoader>*/}
+    </TableLayout>
+  );
+};
+
+export default ParametersPage;
