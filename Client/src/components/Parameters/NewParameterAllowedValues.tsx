9af95f6f313007414edc725a8d7a26dcd593af4f
Hubert Wajda <hubertwajda@Huberts-MacBook-Air.local>
2025-06-24 10:47:03 +0200

    form for text parameter

---
+++ Client/src/components/Parameters/NewParameterAllowedValues.tsx
@@ -0,0 +1,55 @@
+import IconButton from "@components/Shared/IconButton.tsx";
+import {MinusIcon, PlusIcon} from "@heroicons/react/24/outline";
+import {TextParameterRequest} from "@api/models/Parameters.ts";
+import InputField from "@components/Shared/InputField.tsx";
+
+
+type NewParameterAllowedValuesProps = {
+    parameterRequest: TextParameterRequest;
+    increaseAllowedValues: () => void;
+    decreaseAllowedValues: () => void;
+    setAllowedValue: (index: number, value: string) => void;
+}
+
+const NewParameterAllowedValues = (
+  {
+      parameterRequest,
+      increaseAllowedValues,
+      decreaseAllowedValues,
+      setAllowedValue
+  }: NewParameterAllowedValuesProps) => {
+    return (
+      <div>
+          <div className="flex justify-center py-2 gap-2">
+              <IconButton onClick={decreaseAllowedValues}
+                          className="h-[40px] flex bg-white items-center gap-1 !hover:border-red-200"
+                          disabled={parameterRequest.allowedValues.length == 1}
+              >
+                  <MinusIcon className="h-4"/>
+              </IconButton>
+              <div className="flex flex-col justify-center">
+                  {parameterRequest.allowedValues.length}
+              </div>
+              <IconButton onClick={increaseAllowedValues}
+                          className="h-[40px] flex bg-white items-center gap-1 !hover:border-red-200"
+
+              >
+                  <PlusIcon className="h-4"/>
+              </IconButton>
+          </div>
+          <div className="flex flex-col">
+              {parameterRequest.allowedValues.map((value, index) => (
+                <InputField
+                  key={index}
+                  label={`Value ${index + 1} `}
+                  value={value}
+                  onChange={(e) => setAllowedValue(index, e.currentTarget.value)}
+                  validationInfo="Parameter unit must be between 1 and 50 characters long"
+                />
+              ))}
+          </div>
+      </div>
+    );
+};
+
+export default NewParameterAllowedValues;
---
+++ Client/src/components/Parameters/NewParameterForm.tsx
@@ -0,0 +1,97 @@
+import InputField from "@components/Shared/InputField.tsx";
+import {DialogButton} from "@components/Shared/DialogComp.tsx";
+import {AllParametersRequest} from "@api/models/Parameters.ts";
+import {LabeledSelect, SelectItem} from "@components/Shared/LabeledSelect.tsx";
+import {useState} from "react";
+import NewParameterAllowedValues from "@components/Parameters/NewParameterAllowedValues.tsx";
+
+const NewParameterForm = () => {
+
+    const [parameterRequest, setParameterRequest] = useState<AllParametersRequest>({
+        $type: "integer",
+        name: "",
+        unit: ""
+    });
+    const [isRequestValid] = useState(true);
+
+    const addAllowedValue = () => {
+        if (parameterRequest.$type == "text")
+            setParameterRequest({...parameterRequest, allowedValues: [...parameterRequest.allowedValues, ""]})
+    }
+
+    const deleteAllowedValue = () => {
+        if (parameterRequest.$type == "text") {
+            const newValues = parameterRequest.allowedValues.slice(0, -1);
+            setParameterRequest({...parameterRequest, allowedValues: newValues});
+        }
+    }
+
+    const setAllowedValue = (index: number, value: string) => {
+        if (parameterRequest.$type == "text") {
+            const newValues = [...parameterRequest.allowedValues];
+            newValues[index] = value;
+            setParameterRequest({...parameterRequest, allowedValues: newValues});
+        }
+    }
+
+
+    const handleSubmit = () => {
+        console.log("Submit", parameterRequest);
+    }
+
+    return (
+      <div className="flex flex-col">
+          <div className="gap-3">
+              <InputField
+                label="Name"
+                value={parameterRequest.name}
+                onChange={(e) =>
+                  setParameterRequest({...parameterRequest, name: e.currentTarget.value})}
+                isValid={isRequestValid}
+                validationInfo="Parameter name must be between 3 and 50 characters long"
+              />
+              <LabeledSelect
+                label="Type"
+                value={parameterRequest.$type}
+                onChange={(value) => {
+                    if (!value) return;
+                    if (value === "text")
+                        setParameterRequest({$type: value, name: parameterRequest.name, allowedValues: [""]});
+                    else
+                        setParameterRequest({$type: value, name: parameterRequest.name, unit: ""});
+                }}
+              >
+                  <SelectItem value="integer" displayValue="Integer"/>
+                  <SelectItem value="decimal" displayValue="Deciaml"/>
+                  <SelectItem value="text" displayValue="Text"/>
+              </LabeledSelect>
+              {parameterRequest.$type !== "text" &&
+                  <InputField
+                      label="Unit"
+                      value={parameterRequest.unit}
+                      onChange={(e) =>
+                        setParameterRequest({...parameterRequest, unit: e.currentTarget.value})}
+                      isValid={isRequestValid}
+                      validationInfo="Parameter unit must be between 1 and 50 characters long"
+                  />
+              }
+              {parameterRequest.$type === "text" &&
+                  <NewParameterAllowedValues
+                      parameterRequest={parameterRequest}
+                      increaseAllowedValues={addAllowedValue}
+                      decreaseAllowedValues={deleteAllowedValue}
+                      setAllowedValue={setAllowedValue}
+                  />
+              }
+          </div>
+          <DialogButton
+            className="hover:border-green-400"
+            onClick={handleSubmit}
+          >
+              Add Parameter
+          </DialogButton>
+      </div>
+    );
+};
+
+export default NewParameterForm;
