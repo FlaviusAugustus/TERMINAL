71c36538a4a07c03ece3510ef0393583b9386d0a
OGHaruki <jajestemadam@gmail.com>
2025-09-08 20:01:15 +0200

    feature/add-samples-search-new: fixed conditional usage of hooks and code format

--- Client/src/hooks/samples/useSearchSamples.ts
+++ Client/src/hooks/samples/useSearchSamples.ts
@@ -1,26 +1,28 @@
-import {useQuery} from '@tanstack/react-query';
-import apiClient from '@api/apiClient.ts';
-import {SampleDto} from '@api/models/SampleDto.ts';
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "@api/apiClient.ts";
+import { SampleDto } from "@api/models/SampleDto.ts";
 
 export type SamplesSearchParams = {
-    searchPhrase: string;
-    pageNumber: number;
-    pageSize: number;
-}
+  searchPhrase: string;
+  pageNumber: number;
+  pageSize: number;
+};
 
 export type SampleSearchResult = {
-    rows: SampleDto[];
-    pageAmount: number;
-    rowsAmount: number;
-}
+  rows: SampleDto[];
+  pageAmount: number;
+  rowsAmount: number;
+};
 
-async function fetchSamples(params: SamplesSearchParams): Promise<SampleSearchResult> {
-    const response = await apiClient.get('/samples/search', {params});
-    return {
-        rows: response.data.samples,
-        pageAmount: Math.ceil(response.data.totalAmount / params.pageSize),
-        rowsAmount: response.data.totalAmount,
-    };
+async function fetchSamples(
+  params: SamplesSearchParams,
+): Promise<SampleSearchResult> {
+  const response = await apiClient.get("/samples/search", { params });
+  return {
+    rows: response.data.samples,
+    pageAmount: Math.ceil(response.data.totalAmount / params.pageSize),
+    rowsAmount: response.data.totalAmount,
+  };
 }
 
 /**
@@ -33,9 +35,9 @@ async function fetchSamples(params: SamplesSearchParams): Promise<SampleSearchRe
  * @param {SamplesSearchParams} params - The parameters for the samples search request.
  */
 export function useSearchSamples(params: SamplesSearchParams) {
-    return useQuery({
-        queryKey: ['samplesSearch', params],
-        queryFn: () => fetchSamples(params),
-        enabled: !!params.searchPhrase
-    });
+  return useQuery({
+    queryKey: ["samplesSearch", params],
+    queryFn: () => fetchSamples(params),
+    enabled: !!params.searchPhrase,
+  });
 }
--- Client/src/pages/SamplesPage.tsx
+++ Client/src/pages/SamplesPage.tsx
@@ -1,11 +1,11 @@
-import {useState} from "react";
-import {SortingState, PaginationState} from "@tanstack/react-table";
+import { useState } from "react";
+import { PaginationState, SortingState } from "@tanstack/react-table";
 import Samples from "@components/Samples/Samples.tsx";
 import SampleDetails from "@components/Samples/SampleDetails.tsx";
-import {useSamples} from "@hooks/samples/useGetSamples.ts";
-import {useSampleDetails} from "@hooks/samples/useGetSampleDetails.ts";
-import {useDeleteSample} from "@hooks/samples/useDeleteSample.ts";
-import {useSearchSamples} from "@hooks/samples/useSearchSamples.ts";
+import { useSamples } from "@hooks/samples/useGetSamples.ts";
+import { useSampleDetails } from "@hooks/samples/useGetSampleDetails.ts";
+import { useDeleteSample } from "@hooks/samples/useDeleteSample.ts";
+import { useSearchSamples } from "@hooks/samples/useSearchSamples.ts";
 import TableLayout from "./layouts/TableLayout";
 import Loader from "@components/Shared/Loader";
 import ComponentOrLoader from "@components/Shared/ComponentOrLoader";
@@ -13,95 +13,96 @@ import EditSample from "@components/Samples/EditSample";
 import DialogLoader from "@components/Shared/DialogLoader";
 
 const SamplesPage = () => {
-    const [sorting, setSorting] = useState<SortingState>([]);
-    const [pagination, setPagination] = useState<PaginationState>({
-        pageIndex: 0,
-        pageSize: 10,
-    });
-    const [searchPhrase, setSearchPhrase] = useState("");
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [pagination, setPagination] = useState<PaginationState>({
+    pageIndex: 0,
+    pageSize: 10,
+  });
+  const [searchPhrase, setSearchPhrase] = useState("");
 
-    const dataQuerySamples = searchPhrase
-        ? useSearchSamples({
-            searchPhrase,
-            pageNumber: pagination.pageIndex,
-            pageSize: pagination.pageSize,
-        })
-        : useSamples({
-            pageNumber: pagination.pageIndex,
-            pageSize: pagination.pageSize,
-            orderBy: sorting[0]?.id ?? "",
-            desc: sorting[0]?.desc ?? true,
-        });
+  const samplesQuery = useSamples({
+    pageNumber: pagination.pageIndex,
+    pageSize: pagination.pageSize,
+    orderBy: sorting[0]?.id ?? "",
+    desc: sorting[0]?.desc ?? true,
+  });
 
+  const searchSamplesQuery = useSearchSamples({
+    searchPhrase,
+    pageNumber: pagination.pageIndex,
+    pageSize: pagination.pageSize,
+  });
 
-    const deleteMutation = useDeleteSample({
-        pageNumber: pagination.pageIndex,
-        pageSize: pagination.pageSize,
-        orderBy: sorting[0]?.id ?? "",
-        desc: sorting[0]?.desc ?? true,
-    });
+  const dataQuerySamples = searchPhrase ? searchSamplesQuery : samplesQuery;
 
-    const [sampleDetailsId, setSampleDetailsId] = useState<string | null>(null);
-    const [detailsOpen, setDetailsOpen] = useState(false);
-    const [editOpen, setEditOpen] = useState(false);
-    const dataQuerySampleDetails = useSampleDetails(sampleDetailsId);
+  const deleteMutation = useDeleteSample({
+    pageNumber: pagination.pageIndex,
+    pageSize: pagination.pageSize,
+    orderBy: sorting[0]?.id ?? "",
+    desc: sorting[0]?.desc ?? true,
+  });
 
-    const changeSampleDetails = (id: string) => {
-        setSampleDetailsId(id);
-        setDetailsOpen(true);
-    };
+  const [sampleDetailsId, setSampleDetailsId] = useState<string | null>(null);
+  const [detailsOpen, setDetailsOpen] = useState(false);
+  const [editOpen, setEditOpen] = useState(false);
+  const dataQuerySampleDetails = useSampleDetails(sampleDetailsId);
 
-    const editSampleDetails = (id: string) => {
-        setSampleDetailsId(id);
-        setEditOpen(true);
-    };
+  const changeSampleDetails = (id: string) => {
+    setSampleDetailsId(id);
+    setDetailsOpen(true);
+  };
 
-    const handleDelete = async (id: string | null) => {
-        if (!id) return;
-        await deleteMutation.mutateAsync(id);
-    };
+  const editSampleDetails = (id: string) => {
+    setSampleDetailsId(id);
+    setEditOpen(true);
+  };
 
-    return (
-        <TableLayout>
-            <ComponentOrLoader
-                isLoading={dataQuerySamples.isLoading}
-                loader={<Loader/>}
-            >
-                <Samples
-                    samples={dataQuerySamples.data}
-                    sorting={sorting}
-                    pagination={pagination}
-                    setSorting={setSorting}
-                    setPagination={setPagination}
-                    onDelete={handleDelete}
-                    onDetails={changeSampleDetails}
-                    onEdit={editSampleDetails}
-                    searchProps={{
-                        onSearch: setSearchPhrase,
-                        searchValue: searchPhrase,
-                        onClearSearch: () => setSearchPhrase(""),
-                    }}
-                />
-            </ComponentOrLoader>
-            <ComponentOrLoader
-                isLoading={
-                    dataQuerySampleDetails.isLoading || dataQuerySampleDetails.isFetching
-                }
-                loader={<DialogLoader/>}
-            >
-                <EditSample
-                    sample={dataQuerySampleDetails.data}
-                    open={editOpen}
-                    openChange={setEditOpen}
-                />
-                <SampleDetails
-                    sample={dataQuerySampleDetails.data}
-                    open={detailsOpen}
-                    openChange={setDetailsOpen}
-                />
-            </ComponentOrLoader>
-        </TableLayout>
-    );
+  const handleDelete = async (id: string | null) => {
+    if (!id) return;
+    await deleteMutation.mutateAsync(id);
+  };
+
+  return (
+    <TableLayout>
+      <ComponentOrLoader
+        isLoading={dataQuerySamples.isLoading}
+        loader={<Loader />}
+      >
+        <Samples
+          samples={dataQuerySamples.data}
+          sorting={sorting}
+          pagination={pagination}
+          setSorting={setSorting}
+          setPagination={setPagination}
+          onDelete={handleDelete}
+          onDetails={changeSampleDetails}
+          onEdit={editSampleDetails}
+          searchProps={{
+            onSearch: setSearchPhrase,
+            searchValue: searchPhrase,
+            onClearSearch: () => setSearchPhrase(""),
+          }}
+        />
+      </ComponentOrLoader>
+      <ComponentOrLoader
+        isLoading={
+          dataQuerySampleDetails.isLoading || dataQuerySampleDetails.isFetching
+        }
+        loader={<DialogLoader />}
+      >
+        <EditSample
+          sample={dataQuerySampleDetails.data}
+          open={editOpen}
+          openChange={setEditOpen}
+        />
+        <SampleDetails
+          sample={dataQuerySampleDetails.data}
+          open={detailsOpen}
+          openChange={setDetailsOpen}
+        />
+      </ComponentOrLoader>
+    </TableLayout>
+  );
 };
 
 export default SamplesPage;
