5b4ecf26d5ea265baba15d1d9e6ef209b6f30a81
Hubert Wajda <hubertwajda@Huberts-MacBook-Air.local>
2025-09-12 22:58:05 +0200

    refactor AddRecipe

---
+++ Client/src/components/AddRecipe/Steps.tsx
@@ -0,0 +1,68 @@
+import { TabGroup, TabPanel, TabPanels } from "@headlessui/react";
+import StepTabList from "@components/AddRecipe/StepTabList.tsx";
+import ParameterDroppable from "@components/AddRecipe/ParameterDroppable.tsx";
+import {
+  horizontalListSortingStrategy,
+  SortableContext,
+} from "@dnd-kit/sortable";
+import ParameterBox from "@components/AddRecipe/ParameterBox.tsx";
+import { useAddRecipeContext } from "@hooks/useAddRecipeContext.tsx";
+
+const Steps = () => {
+  const { currentStep, setCurrentStep, recipe } = useAddRecipeContext();
+
+  return (
+    <>
+      <div className="p-4 border-b border-gray-200 rounded-t-md">
+        <p>Steps</p>
+      </div>
+      <div className="bg-gray-100 h-full">
+        <TabGroup
+          className="flex flex-col h-full overflow-y-hidden overflow-x-auto"
+          selectedIndex={currentStep == null ? undefined : currentStep}
+          onChange={setCurrentStep}
+        >
+          <StepTabList />
+          <div className="h-full relative">
+            <TabPanels className="h-full overflow-hidden rounded-md p-2">
+              {recipe.steps.map((step, index) => (
+                <TabPanel
+                  key={index}
+                  className="rounded-md h-full bg-white border border-gray-200 shadow-sm w-full p-2 grid grid-cols-10 gap-2"
+                >
+                  <div className="flex flex-col gap-1 w-full col-span-6 overflow-y-auto">
+                    <ParameterDroppable>
+                      <SortableContext
+                        items={step.parameters}
+                        strategy={horizontalListSortingStrategy}
+                      >
+                        {step.parameters.map((parameter) => (
+                          <ParameterBox
+                            key={parameter.id}
+                            parameter={parameter}
+                          />
+                        ))}
+                      </SortableContext>
+                    </ParameterDroppable>
+                  </div>
+                  <div className="flex flex-col gap-1 w-full col-span-4">
+                    <div className="rounded-md border border-gray-200 shadow-sm">
+                      <div className="border-b border-gray-200 rounded-t-md bg-gray-100">
+                        <p className="p-2 text-sm">Comment</p>
+                      </div>
+                      <div className="p-2">
+                        <textarea className="h-auto w-full" rows={20} />
+                      </div>
+                    </div>
+                  </div>
+                </TabPanel>
+              ))}
+            </TabPanels>
+          </div>
+        </TabGroup>
+      </div>
+    </>
+  );
+};
+
+export default Steps;
--- Client/src/pages/AddRecipe.tsx
+++ Client/src/pages/AddRecipe.tsx
@@ -1,19 +1,9 @@
-import { TabGroup, TabPanel, TabPanels } from "@headlessui/react";
 import useGetParameters from "@hooks/parameters/useGetParameters.ts";
-import {
-  horizontalListSortingStrategy,
-  SortableContext,
-} from "@dnd-kit/sortable";
 import { RecipeDragProvider } from "@hooks/useRecipeDragContext";
-import {
-  AddRecipeProvider,
-  useAddRecipeContext,
-} from "@hooks/useAddRecipeContext";
+import { AddRecipeProvider } from "@hooks/useAddRecipeContext";
 import AddRecipeActions from "@components/AddRecipe/AddRecipeActions";
-import ParameterBox from "@components/AddRecipe/ParameterBox";
 import ParameterSelectList from "@components/AddRecipe/ParameterSelectList";
-import ParameterDroppable from "@components/AddRecipe/ParameterDroppable";
-import StepTabList from "@components/AddRecipe/StepTabList";
+import Steps from "@components/AddRecipe/Steps.tsx";
 
 const AddRecipeWithContexts = () => {
   const { data: parameters, isLoading, isError } = useGetParameters();
@@ -33,7 +23,6 @@ const AddRecipeWithContexts = () => {
 
 const AddRecipe = () => {
   const { data: parameters, isLoading, isError } = useGetParameters();
-  const { currentStep, setCurrentStep, recipe } = useAddRecipeContext();
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error...</div>;
@@ -46,54 +35,7 @@ const AddRecipe = () => {
         <AddRecipeActions />
       </div>
       <div className="flex flex-col border border-gray-200 rounded-md bg-white w-full overflow-hidden">
-        <div className="p-4 border-b border-gray-200 rounded-t-md">
-          <p>Recipe</p>
-        </div>
-        <div className="bg-gray-100 h-full">
-          <TabGroup
-            className="flex flex-col h-full overflow-y-hidden overflow-x-auto"
-            selectedIndex={currentStep == null ? undefined : currentStep}
-            onChange={setCurrentStep}
-          >
-            <StepTabList />
-            <div className="h-full relative">
-              <TabPanels className="h-full overflow-hidden rounded-md p-2">
-                {recipe.steps.map((step, index) => (
-                  <TabPanel
-                    key={index}
-                    className="rounded-md h-full bg-white border border-gray-200 shadow-sm w-full p-2 grid grid-cols-10 gap-2"
-                  >
-                    <div className="flex flex-col gap-1 w-full col-span-6 overflow-y-auto">
-                      <ParameterDroppable>
-                        <SortableContext
-                          items={step.parameters}
-                          strategy={horizontalListSortingStrategy}
-                        >
-                          {step.parameters.map((parameter) => (
-                            <ParameterBox
-                              key={parameter.id}
-                              parameter={parameter}
-                            />
-                          ))}
-                        </SortableContext>
-                      </ParameterDroppable>
-                    </div>
-                    <div className="flex flex-col gap-1 w-full col-span-4">
-                      <div className="rounded-md border border-gray-200 shadow-sm">
-                        <div className="border-b border-gray-200 rounded-t-md bg-gray-100">
-                          <p className="p-2 text-sm">Comment</p>
-                        </div>
-                        <div className="p-2">
-                          <textarea className="h-auto w-full" rows={20} />
-                        </div>
-                      </div>
-                    </div>
-                  </TabPanel>
-                ))}
-              </TabPanels>
-            </div>
-          </TabGroup>
-        </div>
+        <Steps />
       </div>
     </div>
   );
