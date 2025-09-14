c98ac16c0eb828a0a9d03e53ff6d78cb3665c9a5
OGHaruki <jajestemadam@gmail.com>
2025-09-14 18:42:04 +0200

    feature/introduce-search-project: removed redutanat comments

--- Client/src/components/Projects/Projects.tsx
+++ Client/src/components/Projects/Projects.tsx
@@ -145,8 +145,8 @@ const Projects = (props: ProjectsProps) => {
           {localSearch && (
             <IconButton
               onClick={() => {
-                setLocalSearch(""); // czyszczenie inputa
-                props.searchProps?.onClearSearch?.(); // wywołanie funkcji clear
+                setLocalSearch("");
+                props.searchProps?.onClearSearch?.();
               }}
               className="h-[40px] flex bg-white items-center gap-1 !hover:border-gray-300"
               title="Clear search"
