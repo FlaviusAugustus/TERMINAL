f813502464d015815595cc87682e2e58cffa1fc7
Hubert Wajda <hubertwajda@Huberts-MacBook-Air.local>
2025-10-03 21:50:37 +0200

    fix sidebar on mobile devices

--- Client/src/components/navbar/Sidebar.tsx
+++ Client/src/components/navbar/Sidebar.tsx
@@ -11,7 +11,7 @@ import SidebarUserProfile from "./SidebarUserProfile.tsx";
  */
 const Sidebar = () => {
   return (
-    <nav className="h-screen min-h-screen max-h-screen min-w-72 p-2 pe-0 overflow-hidden">
+    <nav className="h-screen min-h-screen max-h-screen min-w-72 p-2 pe-0 overflow-hidden pt-20 sm:pt-2">
       <div className="h-full inline-flex w-full flex-col justify-between rounded-md border border-solid bg-white border-gray-200 shadow-sm">
         <div className="navbar-start w-full flex flex-col flex-1 rounded-md bg-white overflow-hidden">
           <div className="flex y-4 items-center bg-white p-4 rounded-t-md">
