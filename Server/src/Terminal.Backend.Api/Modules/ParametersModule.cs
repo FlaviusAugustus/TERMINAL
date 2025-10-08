ce73af6b754b3f4fc0b25c8255a3ad2a87c1b510
Hubert Wajda <hubertwajda@Huberts-MacBook-Air.local>
2025-10-08 14:32:57 +0200

    validate allowedvalues and refactor setting default value

--- Server/src/Terminal.Backend.Api/Modules/ParametersModule.cs
+++ Server/src/Terminal.Backend.Api/Modules/ParametersModule.cs
@@ -27,7 +27,7 @@ public static class ParametersModule
                 ISender sender,
                 CancellationToken ct) =>
             {
-                parameterDto = parameterDto with { Id = ParameterId.Create(), DefaultValue = parameterDto.AllowedValues[0]};
+                parameterDto = parameterDto with { Id = ParameterId.Create() };
                 await sender.Send(new DefineParameterCommand(parameterDto.AsParameter()), ct);
                 return Results.Created(ApiBaseRoute, new { parameterDto.Id });
             }).RequireAuthorization(Permission.ParameterWrite.ToString())
