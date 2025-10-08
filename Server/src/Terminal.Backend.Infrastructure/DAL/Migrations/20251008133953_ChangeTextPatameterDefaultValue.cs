226d30ceb73fa3bcc34bc8dee8bf76eeb4b81bbc
Hubert Wajda <hubertwajda@Huberts-MacBook-Air.local>
2025-10-08 15:43:37 +0200

    fix migration adn compose

---
+++ Server/src/Terminal.Backend.Infrastructure/DAL/Migrations/20251008133953_ChangeTextPatameterDefaultValue.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Terminal.Backend.Infrastructure.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeTextPatameterDefaultValue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "ParameterValues",
+                type: "character varying(21)",
+                maxLength: 21,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Parameters",
+                type: "character varying(21)",
+                maxLength: 21,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TextParameter_DefaultValue",
+                table: "Parameters",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "ParameterValues",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(21)",
+                oldMaxLength: 21);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Parameters",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(21)",
+                oldMaxLength: 21);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "TextParameter_DefaultValue",
+                table: "Parameters",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
