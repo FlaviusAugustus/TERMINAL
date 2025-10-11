413f99cb7af627c8e759299e285769a82db8e904
Hubert Wajda <hubertwajda@Huberts-MacBook-Air.local>
2025-10-11 21:46:59 +0200

    navigate authenticated user to dahsboard

--- Client/src/pages/LoginPage.tsx
+++ Client/src/pages/LoginPage.tsx
@@ -1,6 +1,18 @@
 import LoginForm from "@components/login/LoginForm";
+import { useEffect } from "react";
+import { useIsAuthenticated } from "@hooks/users/auth/useIsAuthenticated.ts";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
+  const isAuthenticated = useIsAuthenticated();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthenticated === true) {
+      navigate("/");
+    }
+  }, []);
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <LoginForm />
