31f3772374d06fbfb8032ac64eb92f25e3bcb251
Gracjan <g.grzech@icloud.com>
2025-10-11 17:51:27 +0200

    Add labeled textarea

---
+++ Client/src/components/shared/form/LabeledTextArea.tsx
@@ -0,0 +1,19 @@
+import { Textarea, TextareaProps } from "@headlessui/react";
+import LabeledField from "./LabeledField";
+
+type LabeledTextAreaProps = TextareaProps & {
+  label: string;
+};
+
+const LabeledTextArea = ({ label, ...rest }: LabeledTextAreaProps) => {
+  return (
+    <LabeledField label={label}>
+      <Textarea
+        {...rest}
+        className="w-full px-3 py-2 border rounded-md focus:ring-2 focus:outline-none focus:ring-blue-500 focus:ring-offset-2"
+      />
+    </LabeledField>
+  );
+};
+
+export default LabeledTextArea;
