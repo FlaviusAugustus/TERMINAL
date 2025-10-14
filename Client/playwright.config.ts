5c704190f6eeb2921e83a65b2ba2c0fa1d93804e
OGHaruki <jajestemadam@gmail.com>
2025-10-14 20:56:12 +0200

    fixed playwright config

--- Client/playwright.config.ts
+++ Client/playwright.config.ts
@@ -69,7 +69,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run dev",
-    url: "http://localhost:5173",
+    url: "https://localhost:5173",
     reuseExistingServer: !process.env.CI,
   },
 });
