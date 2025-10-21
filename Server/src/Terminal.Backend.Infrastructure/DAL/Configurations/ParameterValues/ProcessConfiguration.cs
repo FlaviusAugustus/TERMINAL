1115faec1db3965b8c7809eb19e6a4dab6f99a87
Tomasz Goluch <goluch@eti.pg.edu.pl>
2025-10-21 14:42:23 +0200

    Added ProcessConfiguration.cs

---
+++ Server/src/Terminal.Backend.Infrastructure/DAL/Configurations/ParameterValues/ProcessConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Terminal.Backend.Core.Entities;
+
+public class ProcessConfiguration : IEntityTypeConfiguration<Process>
+{
+    public void Configure(EntityTypeBuilder<Process> builder)
+    {
+
+        builder.HasKey(p => p.Id);
+
+        var codeConverter = new ValueConverter<Code, string>(
+            code => code.ToString(),
+            value => Code.Create(value)
+        );
+
+        builder.Property(p => p.CodeNumber)
+               .HasConversion(codeConverter)
+               .HasMaxLength(7)
+               .IsRequired();
+    }
+}
