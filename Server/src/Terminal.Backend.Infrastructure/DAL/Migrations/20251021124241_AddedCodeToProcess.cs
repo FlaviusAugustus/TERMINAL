1ccece7ed7135d23eeb887a8a02d0feda70808cc
Tomasz Goluch <goluch@eti.pg.edu.pl>
2025-10-21 14:43:22 +0200

    Added AddedCodeToProcess migration

---
+++ Server/src/Terminal.Backend.Infrastructure/DAL/Migrations/20251021124241_AddedCodeToProcess.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Terminal.Backend.Infrastructure.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCodeToProcess : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CodeNumber",
+                table: "Processes",
+                type: "character varying(7)",
+                maxLength: 7,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CodeNumber",
+                table: "Processes");
+        }
+    }
+}
