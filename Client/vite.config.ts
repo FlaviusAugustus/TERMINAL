cc64cd58421bfcec688134357a4c42a25e456606
Gracjan <g.grzech@icloud.com>
2025-11-08 13:27:38 +0100

    Make our app a PWA

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
@@ -7,6 +8,33 @@ import viteTsConfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [
     react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ["favicon.ico", "512x512.png", "192x192.png"],
+      manifest: {
+        name: "Terminal",
+        short_name: "Terminal",
+        description: "Manage processes and recipes",
+        start_url: "/",
+        background_color: "#ffffff",
+        display: "fullscreen",
+        icons: [
+          {
+            src: "192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
     basicSsl({
       name: "test",
       domains: ["localhost"],
