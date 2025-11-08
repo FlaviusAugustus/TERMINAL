585fe450641a139d9034f95defb1e59f63e65b7a
Gracjan <g.grzech@icloud.com>
2025-11-08 13:41:29 +0100

    Add online/offline indicator

--- Client/src/components/navbar/SidebarUserProfile.tsx
+++ Client/src/components/navbar/SidebarUserProfile.tsx
@@ -4,6 +4,8 @@ import { toastPromise } from "@utils/toast.utils.tsx";
 import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import { useLogoutMutation } from "@hooks/users/auth/useLogoutMutation.ts";
+import useIsOnline from "@hooks/useIsOnline";
+import clsx from "clsx";
 
 function getUserRoleDisplayValue(role: string): string {
   if (role === "Administrator") return "Administrator";
@@ -40,6 +42,7 @@ const SidebarUserProfile = () => {
   const { data, status } = useUserData();
   const navigate = useNavigate();
   const logoutMutation = useLogoutMutation();
+  const online = useIsOnline();
 
   const handleLogout = useCallback(async () => {
     await toastPromise(logoutMutation.mutateAsync(), {
@@ -56,11 +59,19 @@ const SidebarUserProfile = () => {
   return (
     <div className="p-2 bg-white md:rounded-b-md w-full">
       <div className="flex gap-3 rounded-md p-2 hover:bg-gray-200 group hover:cursor-pointer w-full items-center">
-        <div
-          style={{ backgroundColor: getAvatarColor(data?.email) }}
-          className="flex text-white w-10 h-10 rounded-md justify-center items-center noinvert"
-        >
-          <span>{getEmailInitials(data?.email)}</span>
+        <div className="relative">
+          <div
+            style={{ backgroundColor: getAvatarColor(data?.email) }}
+            className="flex text-white w-10 h-10 rounded-md justify-center items-center noinvert"
+          >
+            <span>{getEmailInitials(data?.email)}</span>
+          </div>
+          <div
+            className={clsx(
+              "absolute z-10 h-2 w-2 outline border outline-white border-black/10 rounded-full -top-0.5 -right-0.5",
+              online ? "bg-green-400" : "bg-red-400"
+            )}
+          ></div>
         </div>
         <div className="flex flex-col justify-start">
           <p className="text-sm w-full text-left">{data?.email}</p>
