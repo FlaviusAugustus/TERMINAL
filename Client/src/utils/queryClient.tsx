73df2c5135402535e99940f70a53991d86c93f74
Gracjan <g.grzech@icloud.com>
2025-11-09 19:08:05 +0100

    Remove redundant files

--- Client/src/utils/queryClient.tsx
+++ Client/src/utils/queryClient.tsx
@@ -1,6 +1,5 @@
-import { QueryClient, QueryCache } from "@tanstack/react-query";
+import { QueryClient } from "@tanstack/react-query";
 import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister";
-import { toastNotify } from "./toast.utils.tsx";
 
 export const queryClient = new QueryClient({
   defaultOptions: {
