f090674c46d0ec7f1e927ec20f10705fd110a116
Hubert Wajda <hubert.wajda03@gmail.com>
2025-11-24 14:19:42 +0100

    prevent from client evaluation

--- Server/src/Terminal.Backend.Application/DTO/Processes/GetGroupedByDaysProcessesDto.cs
+++ Server/src/Terminal.Backend.Application/DTO/Processes/GetGroupedByDaysProcessesDto.cs
@@ -4,5 +4,5 @@ public class GetGroupedByDaysProcessesDto
 {
     public Dictionary<string, List<ProcessDto>> GroupedByDaysProcesses = new Dictionary<string, List<ProcessDto>>();
     
-    public sealed record ProcessDto(Guid Id, Code Code, IEnumerable<string> Projects, string CreatedAtUtc, string Comment);
+    public sealed record ProcessDto(Guid Id, Code Code, IEnumerable<string> Projects, string Comment);
 }
--- Server/src/Terminal.Backend.Infrastructure/DAL/Handlers/Processes/GetGroupedByDaysProcessesHandler.cs
+++ Server/src/Terminal.Backend.Infrastructure/DAL/Handlers/Processes/GetGroupedByDaysProcessesHandler.cs
@@ -20,23 +20,31 @@ internal sealed class GetGroupedByDaysProcessesHandler:
         CancellationToken cancellationToken)
     {
         var dateLimit = DateTime.UtcNow.AddDays(-request.Days).Date;
-        
+
         var processes = _processes
             .AsNoTracking()
             .Where(p => dateLimit <= p.CreatedAtUtc.Date)
-            .Select(p => new GetGroupedByDaysProcessesDto.ProcessDto(
-                p.Id, 
+            .Select(p => new
+            {
+                p.Id,
                 p.Code,
-                p.Projects.Select(p => p.Name),
-                p.CreatedAtUtc.ToString("MM/dd/yyyy"),
-                p.Comment));
-        
-        return new ()
+                p.CreatedAtUtc.Date,
+                Projects = p.Projects.Select(p => p.Name),
+                p.Comment
+            })
+            .GroupBy(p => p.Date);
+
+    return new ()
         {
-            GroupedByDaysProcesses = processes
-                .ToList()
-                .GroupBy(p => p.CreatedAtUtc)
-                .ToDictionary(p => p.Key, p=> p.ToList())
+            GroupedByDaysProcesses = processes.ToDictionary(
+                p => p.Key.ToString("MM/dd/yyyy"), 
+                p => 
+                    p.Select(p => new GetGroupedByDaysProcessesDto.ProcessDto(
+                            p.Id, 
+                            p.Code,
+                            p.Projects,
+                            p.Comment))
+                    .ToList())
         };
     }
     
