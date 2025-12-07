54e585b5880adc9167e13e89b8e0408444018600
Hubert Wajda <hubert.wajda03@gmail.com>
2025-12-07 21:24:49 +0100

    validate prefix

--- Server/src/Terminal.Backend.Core/Entities/PrefixCounter.cs
+++ Server/src/Terminal.Backend.Core/Entities/PrefixCounter.cs
@@ -1,4 +1,5 @@
-﻿using Terminal.Backend.Core.ValueObjects;
+﻿using Terminal.Backend.Core.Exceptions;
+using Terminal.Backend.Core.ValueObjects;
 
 namespace Terminal.Backend.Core.Entities;
 
@@ -14,9 +15,9 @@ public class PrefixCounter
     {
         if (string.IsNullOrWhiteSpace(prefix))
         {
-            throw new ArgumentException("Prefix nie może być pusty.", nameof(prefix));
+            throw new ArgumentException("Prefix cannot be empty", nameof(prefix));
         }
-        Prefix = prefix;
+        Prefix = validatedPrefix(prefix);
         LastValue = 0; 
     }
     public int Increment()
@@ -35,6 +36,30 @@ public class PrefixCounter
     
     public void Update(string prefix)
     {
-        Prefix = prefix;
+        Prefix = validatedPrefix(prefix);
+    }
+
+    private string validatedPrefix(string prefix)
+    {
+        if (string.IsNullOrWhiteSpace(prefix))
+        {
+            throw new InvalidCodeFormatException("Prefix cannot be null or whitespace.");
+        }
+
+        string normalizedPrefix = prefix.ToUpper();
+
+        if (!normalizedPrefix.EndsWith("X"))
+        {
+            throw new InvalidCodeFormatException("Prefix must end with 'X'.");
+        }
+
+        foreach (char c in normalizedPrefix)
+        {
+            if (c < 'A' || c > 'Z')
+            {
+                throw new InvalidCodeFormatException("Prefix can only contain letters.");
+            }
+        }
+        return normalizedPrefix;
     }
 }
